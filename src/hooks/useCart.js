73d51449267c52686e5aafe59cde53799749a931
nathanielsean88-afk <nathanielsean88@gmail.com>
2026-05-27 07:23:49 +0700

    Add files via upload

---
+++ src/hooks/useCart.js
@@ -0,0 +1,6 @@
+// useCart.js
+// Custom hook wrapper untuk CartContext
+// Re-export dari context agar import lebih clean:
+// import useCart from '../hooks/useCart'
+
+export { useCart as default } from '../context/CartContext.jsx'
---
+++ src/hooks/useTransaction.js
@@ -0,0 +1,6 @@
+// useTransaction.js
+// Custom hook wrapper untuk TransactionContext
+// Re-export dari context agar import lebih clean:
+// import useTransaction from '../hooks/useTransaction'
+
+export { useTransaction as default } from '../context/TransactionContext.jsx'
